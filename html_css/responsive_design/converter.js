de4821e57530cb619c7fa71a9ee80905d9b22c8a
afrankl <afrankl@berkeley.edu>
2016-02-09 21:51:43 -0800

    Almost done with the assignment

--- html_css/responsive_design/converter.js
+++ html_css/responsive_design/converter.js
@@ -1,5 +1,56 @@
-var currency_input = document.getElementById('currency-input');
-var currency_output = document.getElementById('currency-output');
-function convertCurrency() {
-	 currency_output.innerHTML = parseFloat(currency_input.value) * 1.3;
+var title = document.getElementById("conversion-title");
+var currency_types = ["Dollars", "Euros"];
+var language_types = ["English", "Spanish"];
+var bit_types = ["Kilo", "Mega", "Giga"];
+
+var input = document.getElementById('input-type');
+var input_dropdown = document.getElementById('dropdown-input');
+var output = document.getElementById('output-type');
+var output_dropdown = document.getElementById('dropdown-output');
+var input_selected = "Dollars";
+var output_selected = "Euros";
+
+function create_dropdown(types, dropdown) {
+	for (var i = 0; i < types.length; i++) {
+		var t = types[i];
+		var item = document.createElement("div");
+		item.innerHTML = t;
+		dropdown.appendChild(item);
+	}
 }
+
+function destroy_dropdown(dropdown) {
+	while (dropdown.childNodes.length > 0) {
+		dropdown.removeChild(dropdown.childNodes[0]);
+	}
+}
+
+function reset_dropdowns(types) {
+	destroy_dropdown(input_dropdown);
+	destroy_dropdown(output_dropdown);
+	create_dropdown(types, input_dropdown);
+	create_dropdown(types, output_dropdown);
+}
+
+function converter(newTitle, types) {
+	title.innerHTML = newTitle;
+	reset_dropdowns(types);
+}
+
+function currency() {
+	converter("Currency Converter", currency_types);
+}
+
+function languages() {
+	converter("Language Converter", language_types);
+}
+
+function bits() {
+	converter("Bit Conversion", bit_types);
+}
+
+function bytes() {
+	converter("Byte Conversion", bit_types);
+}
+
+
